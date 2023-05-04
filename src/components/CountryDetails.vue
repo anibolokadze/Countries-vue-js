d2b1bad2d6d162c5b96d768d9599f09d46a4f227
dimitribolokadze <anibolokadze@gmail.com>
2023-05-04 12:51:24 +0400

    router page

--- src/components/CountryDetails.vue
+++ src/components/CountryDetails.vue
@@ -1,45 +1,4 @@
 <template>
-  <div v-if="!loading">
-    <h1>{{ country.name && country.name.official }}</h1>
-  </div>
-  <div v-else>
-    <p>Loading...</p>
-  </div>
+  <div></div>
 </template>
-
-<script>
-import axios from "axios";
-
-export default {
-  name: "CountryDetails",
-  filters: {
-    formatLanguages(languages) {
-      return Object.values(languages).join(", ");
-    },
-  },
-  data() {
-    return {
-      country: {},
-      loading: true,
-    };
-  },
-  // created() {
-  //   const name = this.$route.params.name;
-  //   this.fetchCountry(this.$route.params.name);
-  // },
-  methods: {
-    fetchCountry() {
-      const name = this.$route.params.name;
-      axios
-        .get(`https://restcountries.com/v3.1/name/${name}`)
-        .then((response) => {
-          this.country = response.data[0];
-          this.loading = false;
-        })
-        .catch(() => {
-          this.loading = false;
-        });
-    },
-  },
-};
-</script>
+>
